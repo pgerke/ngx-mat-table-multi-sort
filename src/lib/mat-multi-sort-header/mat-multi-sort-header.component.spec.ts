315229d7932a2f03f2b4ab70255c5e257d69d401
Philip Gerke <me@philipgerke.com>
2025-01-03 11:28:11 +0100

    #4 test: Provide unit test coverage

--- src/lib/mat-multi-sort-header/mat-multi-sort-header.component.spec.ts
+++ src/lib/mat-multi-sort-header/mat-multi-sort-header.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { MatSort } from "@angular/material/sort";
+import { MatSort, SortDirection } from "@angular/material/sort";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { MatMultiSortDirective } from "../mat-multi-sort.directive";
 import { MatMultiSortHeaderComponent } from "./mat-multi-sort-header.component";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 describe("MatMultiSortHeaderComponent", () => {
   let component: MatMultiSortHeaderComponent;
@@ -30,6 +30,7 @@ describe("MatMultiSortHeaderComponent", () => {
   });
 
   it("should not trigger sort when the header is disabled", () => {
+    sort.disabled = false;
     component.disabled = true;
     const spy = spyOn(sort, "sort");
     component._toggleOnInteraction();
@@ -50,5 +51,24 @@ describe("MatMultiSortHeaderComponent", () => {
     const spy = spyOn(sort, "sort");
     component._toggleOnInteraction();
     expect(spy).toHaveBeenCalled();
+  });
+
+  [
+    { direction: "asc", aria: "ascending" },
+    { direction: "desc", aria: "descending" },
+  ].forEach((test) => {
+    it(`should behave correctly when sorted ${test.aria}ly`, () => {
+      component.id = "test";
+      sort._sorts.push({
+        active: "test",
+        direction: test.direction as SortDirection,
+      });
+      component._updateArrowDirection();
+      expect(component._arrowDirection).toBe(test.direction);
+      expect(component.sortDirection).toBe(test.direction);
+      expect(component.sortIndex).toBe(0);
+      expect(component._isSorted()).toBe(true);
+      expect(component._getAriaSortAttribute()).toBe(test.aria);
+    });
   });
 });
