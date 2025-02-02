6d198609a7e4df35f3fb4f5b9d30a4b7f7e009af
Philip Gerke <me@philipgerke.com>
2025-02-02 14:45:08 +0100

    #36 fix: Fix unit test
    Signed-off-by: Philip Gerke <me@philipgerke.com>

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -159,9 +159,7 @@ describe("AppComponent", () => {
       { active: "score", direction: "desc" },
     ]);
 
-    component.onPersistenceModeChanged({
-      event: { value: "Custom_2" } as MatRadioChange,
-    });
+    component.onPersistenceModeChanged({ value: "Custom_2" } as MatRadioChange);
     fixture.detectChanges(); // Trigger change detection cycle to apply effects
     expect(setItemSpy).toHaveBeenCalledWith("persistenceMode", "Custom_2");
     expect(setItemSpy).toHaveBeenCalledWith(
